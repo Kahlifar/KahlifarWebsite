98d93ea927f40c32f4c1129ecbc6d86f975d3c3b
fwesterath <fwesterath@ethz.ch>
2022-08-31 16:51:36 +0200

    ajust anvbar

---
+++ components/ServerProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="server-profile">
+    <div class="server-profile__title">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props
+}
+</script>
+
+<style>
+
+</style>
