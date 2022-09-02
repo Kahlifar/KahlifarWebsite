7d17bf195112de9b85f70828c6a13e5e065f6e25
Fynnyx <westerathf@gmail.com>
2022-09-02 11:11:54 +0200

    add sitemap for google

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,11 +58,24 @@ export default {
     '@nuxtjs/style-resources',
     '@nuxt/content',
     '@nuxtjs/axios',
+    '@nuxtjs/sitemap',
   ],
 
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
+  },
+  sitemap: {
+    hostname: 'https://kahlifar.de',
+    gzip: true,
+    exclude: [
+      '/admin/**'
+    ],
+    routes: [
+      '/server',
+      '/team',
+      '/socials',
+    ]
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
