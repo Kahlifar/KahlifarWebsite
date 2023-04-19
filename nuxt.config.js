f85d5cf879171169e05452157b769b8f2c5ff598
Westerath  Fynn (ID) <fwesterath@ethz.ch>
2023-04-19 15:40:21 +0200

    update meta image

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
       },
       {
         property: 'og:image',
-        content: require("~/assets/images/logo_background.png"),
+        content: process.env.BASE_URL + '/assets/images/logo_background.png',
       },
       { name: 'msapplication-TileColor', content: '#023047' },
       { name: 'theme-color', content: '#023047' },
