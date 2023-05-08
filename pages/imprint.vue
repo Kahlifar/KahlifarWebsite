90b4e39fd1922fa0414734e19322909253434f31
Westerath  Fynn (ID) <fwesterath@ethz.ch>
2023-05-08 15:08:14 +0200

    mobile fixes, add article, pp, imprint page, new footer, navbar

---
+++ pages/imprint.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="default-content">
+    <h1>Imprint</h1>
+    <p>Responsible for the content of this website is:</p>
+    <p>
+      Kahlifar Community<br />
+      Mail: <a href="mailto:info@kahlifar.de">info@kahlifar.de</a> <br>
+      Fynnyx#4024 (<a href="/discord">Discord</a>)
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
